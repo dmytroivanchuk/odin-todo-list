66ab34158aff472c00057edfbc367ac6f76cb802
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-07-04 16:45:05 +0300

    feat: configure and populate Storage class
    Add starting lists and projects to Storage to
    guide users on how to use an application.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,68 @@
 import "./preflight.css";
 import "./index.css";
+import Storage from "./storage";
+import ToDo from "./to-do";
+import Project from "./project";
+import List from "./list";
 
-console.log("Hello World!");
+const storage = new Storage();
+initializeInbox();
+initializeStartingProject();
+
+function initializeInbox() {
+  const inbox = new List();
+  inbox.title = "Inbox";
+  storage.addList(inbox);
+}
+
+function initializeStartingProject() {
+  const doubleClickToDo = new ToDo();
+  doubleClickToDo.title = "Double-click this to-do";
+  doubleClickToDo.description =
+    "You’re looking at a to-do! Complete it by clicking the checkbox on the left of this to-do.";
+
+  const createToDo = new ToDo();
+  createToDo.title = "Create a new to-do";
+  createToDo.description =
+    "Close current to-do by clicking on the blank space outside of sidebar. Then click the plus icon on the bottom of the page.";
+
+  const deadlineToDo = new ToDo();
+  deadlineToDo.title = "Set a deadline for this to-do";
+  deadlineToDo.description =
+    "Click the flag icon on the bottom right of this to-do.";
+
+  const priorityToDo = new ToDo();
+  priorityToDo.title = "Set a priority for this to-do";
+  priorityToDo.description =
+    "Click the exclamation mark icon on the bottom right of this to-do";
+
+  const checklistToDo = new ToDo();
+  checklistToDo.title = "Add a checklist for this to-do";
+  checklistToDo.description =
+    "Click the checklist icon on the bottom right of this to-do";
+
+  const createProjectToDo = new ToDo();
+  createProjectToDo.title = "Create a project";
+  createProjectToDo.description =
+    "On to bigger things! At the bottom of the sidebar, click “+ New Project” to add a project of your own.";
+
+  const doneToDo = new ToDo();
+  doneToDo.title = "You're done!";
+  doneToDo.description =
+    "That’s all you really need to know. Feel free to start adding your own projects and to-dos.";
+
+  const startingProject = new Project();
+  startingProject.title = "Learn the basics";
+  startingProject.description =
+    "This project shows you everything you need to know to hit the ground running.";
+  startingProject.toDos = [
+    doubleClickToDo,
+    createToDo,
+    deadlineToDo,
+    priorityToDo,
+    checklistToDo,
+    createProjectToDo,
+    doneToDo,
+  ];
+  storage.addProject(startingProject);
+}
---
+++ src/storage.js
@@ -0,0 +1,16 @@
+export default class Storage {
+  #lists = [];
+  #projects = [];
+
+  addList(list) {
+    this.#lists.push(list);
+    const listsJSON = JSON.stringify(this.#lists);
+    localStorage.setItem("lists", listsJSON);
+  }
+
+  addProject(project) {
+    this.#projects.push(project);
+    const projectsJSON = JSON.stringify(this.#projects);
+    localStorage.setItem("projects", projectsJSON);
+  }
+}
