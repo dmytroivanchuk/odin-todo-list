cd769bd1e1f8cc0480e6b6594f8fc58989f93971
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-07-09 12:19:20 +0300

    feat: add blue background to to-do when selected

--- src/components/content/project/to-do-title/to-do-title.js
+++ src/components/content/project/to-do-title/to-do-title.js
@@ -4,13 +4,21 @@ export default function createToDoTitleComponent({ done, title }) {
   const toDoTitleContainer = document.createElement("div");
   toDoTitleContainer.classList.add("to-do-title-container");
   const toDoDone = document.createElement("input");
+  toDoDone.classList.add("to-do-done");
   toDoDone.type = "checkbox";
-  toDoDone.id = "to-do-done";
   toDoDone.checked = done;
   const toDoTitle = document.createElement("label");
   toDoTitle.classList.add("to-do-title");
-  toDoTitle.htmlFor = toDoDone.id;
   toDoTitle.textContent = title;
+  toDoTitle.addEventListener("click", () => {
+    const containers = document.querySelectorAll(".to-do-title-container");
+    containers.forEach((container) => {
+      if (container.classList.contains("background-blue")) {
+        container.classList.remove("background-blue");
+      }
+    });
+    toDoTitleContainer.classList.add("background-blue");
+  });
   toDoTitleContainer.append(toDoDone, toDoTitle);
   return toDoTitleContainer;
 }
