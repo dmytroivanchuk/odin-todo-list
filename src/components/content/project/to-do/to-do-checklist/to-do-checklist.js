6855d5ae51fbe01f175a57a8cdf586434c0f4b27
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-10-08 15:29:03 +0300

    feat: configure ToDoChecklist component

--- src/components/content/project/to-do/to-do-checklist/to-do-checklist.js
+++ src/components/content/project/to-do/to-do-checklist/to-do-checklist.js
@@ -1,16 +1,47 @@
+import "./to-do-checklist.css";
+import removeIcon from "../../../../../assets/remove.svg";
+
 export default function createToDoChecklist(checklist) {
   const toDoChecklist = document.createElement("div");
   toDoChecklist.classList.add("to-do-checklist");
   checklist.forEach(checklistItem => {
+    const toDoChecklistTitle = document.createElement("div");
+    toDoChecklistTitle.classList.add("to-do-checklist-title");
     const toDoChecklistDone = document.createElement("input");
     toDoChecklistDone.type = "checkbox";
     toDoChecklistDone.classList.add("to-do-checklist-done");
-    toDoChecklistDone.checked = checklist.done;
-    const toDoChecklistTitle = document.createElement("div");
-    toDoChecklistTitle.classList.add("to-do-checklist-title");
-    toDoChecklistTitle.contentEditable = "true";
-    toDoChecklistTitle.textContent = checklist.title;
-    toDoChecklist.append(toDoChecklistDone, toDoChecklistTitle);
+    toDoChecklistDone.checked = checklistItem.done;
+    toDoChecklistDone.addEventListener("change", () => {
+      if (toDoChecklistDone.checked) {
+        toDoChecklistTitleTitle.classList.add("foreground-gray");
+      } else {
+        toDoChecklistTitleTitle.classList.remove("foreground-gray");
+      }
+    });
+
+    const toDoChecklistTitleTitle = document.createElement("div");
+    toDoChecklistTitleTitle.classList.add("to-do-checklist-title-title");
+    toDoChecklistTitleTitle.contentEditable = "true";
+    toDoChecklistTitleTitle.textContent = checklistItem.title;
+    if (checklistItem.done) {
+      toDoChecklistTitleTitle.classList.add("foreground-gray");
+    }
+    toDoChecklistTitleTitle.addEventListener("focusin", () => {
+      toDoChecklistTitle.classList.add("active");
+    });
+    toDoChecklistTitleTitle.addEventListener("focusout", () => {
+      toDoChecklistTitle.classList.remove("active");
+    });
+
+    const toDoChecklistRemove = document.createElement("button");
+    toDoChecklistRemove.classList.add("to-do-checklist-remove");
+    const toDoChecklistRemoveIcon = document.createElement("img");
+    toDoChecklistRemoveIcon.classList.add("to-do-checklist-remove-icon");
+    toDoChecklistRemoveIcon.src = removeIcon;
+    toDoChecklistRemove.append(toDoChecklistRemoveIcon);
+
+    toDoChecklistTitle.append(toDoChecklistDone, toDoChecklistTitleTitle, toDoChecklistRemove);
+    toDoChecklist.append(toDoChecklistTitle);
   });
 
   return toDoChecklist;
