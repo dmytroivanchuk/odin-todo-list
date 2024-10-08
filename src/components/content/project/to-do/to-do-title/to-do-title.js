4739d8ce439b355829f93ae2960ddfb571918d26
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-10-08 15:28:20 +0300

    feat: configure ToDoTitle component

--- src/components/content/project/to-do/to-do-title/to-do-title.js
+++ src/components/content/project/to-do/to-do-title/to-do-title.js
@@ -8,18 +8,11 @@ export default function createToDoTitle(done, title) {
   toDoTitleDone.classList.add("to-do-title-done");
   toDoTitleDone.type = "checkbox";
   toDoTitleDone.checked = done;
-  const toDoTitleTitle = document.createElement("label");
+  const toDoTitleTitle = document.createElement("div");
   toDoTitleTitle.classList.add("to-do-title-title");
+  toDoTitleTitle.classList.add("cursor-default");
+  toDoTitleTitle.dataset.placeholder = "New To-Do";
   toDoTitleTitle.textContent = title;
-  toDoTitleTitle.addEventListener("click", () => {
-    const titles = document.querySelectorAll(".to-do-title");
-    titles.forEach((title) => {
-      if (title.classList.contains("background-blue")) {
-        title.classList.remove("background-blue");
-      }
-    });
-    toDoTitle.classList.add("background-blue");
-  });
   toDoTitle.append(toDoTitleDone, toDoTitleTitle);
   return toDoTitle;
 }
