a255cc6659eecbdaffbe8d1723879de984d61a20
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-10-09 10:38:35 +0300

    feat: configure ToDoDeadline component

--- src/components/content/project/to-do/to-do-deadline/to-do-deadline.js
+++ src/components/content/project/to-do/to-do-deadline/to-do-deadline.js
@@ -1,15 +1,22 @@
 import deadlineIcon from "../../../../../assets/deadline.svg"
 import removeIcon from "../../../../../assets/remove.svg"
+import "./to-do-deadline.css"
 
 export default function createToDoDeadline(deadline) {
   const toDoDeadline = document.createElement("div");
   toDoDeadline.classList.add("to-do-deadline");
   const toDoDeadlineIcon = document.createElement("img");
+  toDoDeadlineIcon.classList.add("to-do-deadline-icon");
   toDoDeadlineIcon.src = deadlineIcon;
   const toDoDeadlineTitle = document.createElement("p");
+  toDoDeadlineTitle.classList.add("to-do-deadline-title");
   toDoDeadlineTitle.textContent = deadline;
-  const toDoDeadlineRemove = document.createElement("img");
-  toDoDeadlineRemove.src = removeIcon;
+  const toDoDeadlineRemove = document.createElement("button");
+  toDoDeadlineRemove.classList.add("to-do-deadline-remove");
+  const toDoDeadlineRemoveIcon = document.createElement("img");
+  toDoDeadlineRemoveIcon.classList.add("to-do-deadline-remove-icon");
+  toDoDeadlineRemoveIcon.src = removeIcon;
+  toDoDeadlineRemove.append(toDoDeadlineRemoveIcon);
   toDoDeadline.append(toDoDeadlineIcon, toDoDeadlineTitle, toDoDeadlineRemove);
   return toDoDeadline;
 }
